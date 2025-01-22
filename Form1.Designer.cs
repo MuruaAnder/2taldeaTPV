167d157e6bc47fc22a12fea9caf113e92f8d1610
Ander Murua <and_mur_arr@goierrieskola.org>
2025-01-22 13:26:43 +0100

    produktuk gehitu ta editatu pantalla pantalla max falta, itext7 instalatuta bakarrik pdf falta

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -6,6 +6,8 @@
         private TextBox txtPassword;
         private Button btnLogin;
         private PictureBox pictureBoxLogo;
+        private Label lblName;
+        private Label lblPassword;
 
         private void InitializeComponent()
         {
@@ -14,12 +16,14 @@
             txtPassword=new TextBox();
             btnLogin=new Button();
             pictureBoxLogo=new PictureBox();
+            lblName=new Label();
+            lblPassword=new Label();
             ((System.ComponentModel.ISupportInitialize)pictureBoxLogo).BeginInit();
             SuspendLayout();
             // 
             // txtName
             // 
-            txtName.Location=new Point(244, 302);
+            txtName.Location=new Point(284, 310);
             txtName.Name="txtName";
             txtName.PlaceholderText="Izena";
             txtName.Size=new Size(300, 23);
@@ -27,7 +31,7 @@
             // 
             // txtPassword
             // 
-            txtPassword.Location=new Point(244, 354);
+            txtPassword.Location=new Point(284, 370);
             txtPassword.Name="txtPassword";
             txtPassword.PlaceholderText="Pasahitza";
             txtPassword.Size=new Size(300, 23);
@@ -36,10 +40,10 @@
             // 
             // btnLogin
             // 
-            btnLogin.BackColor=Color.FromArgb(105, 71, 44);
+            btnLogin.BackColor=Color.SaddleBrown;
             btnLogin.FlatStyle=FlatStyle.Popup;
             btnLogin.ForeColor=SystemColors.ButtonHighlight;
-            btnLogin.Location=new Point(244, 406);
+            btnLogin.Location=new Point(284, 420);
             btnLogin.Name="btnLogin";
             btnLogin.Size=new Size(300, 40);
             btnLogin.TabIndex=3;
@@ -50,61 +54,68 @@
             // pictureBoxLogo
             // 
             pictureBoxLogo.Image=(Image)resources.GetObject("pictureBoxLogo.Image");
-            pictureBoxLogo.Location=new Point(244, 46);
+            pictureBoxLogo.Location=new Point(284, 50);
             pictureBoxLogo.Name="pictureBoxLogo";
-            pictureBoxLogo.Size=new Size(183, 148);
+            pictureBoxLogo.Size=new Size(250, 200);
             pictureBoxLogo.SizeMode=PictureBoxSizeMode.Zoom;
             pictureBoxLogo.TabIndex=0;
             pictureBoxLogo.TabStop=false;
+            // 
+            // lblName
+            // 
+            lblName.Font=new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            lblName.Location=new Point(284, 280);
+            lblName.Name="lblName";
+            lblName.Size=new Size(300, 20);
+            lblName.TabIndex=0;
+            lblName.Text="Izena:";
+            lblName.TextAlign=ContentAlignment.MiddleLeft;
+            // 
+            // lblPassword
+            // 
+            lblPassword.Font=new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            lblPassword.Location=new Point(284, 340);
+            lblPassword.Name="lblPassword";
+            lblPassword.Size=new Size(300, 30);
+            lblPassword.TabIndex=2;
+            lblPassword.Text="Pasahitza:";
+            lblPassword.TextAlign=ContentAlignment.MiddleLeft;
             // 
             // Form1
             // 
             AutoScaleDimensions=new SizeF(7F, 15F);
             AutoScaleMode=AutoScaleMode.Font;
-            BackColor=Color.FromArgb(191, 171, 146);
+            BackColor=Color.BurlyWood;
             ClientSize=new Size(784, 561);
-            Controls.Add(pictureBoxLogo);
+            Controls.Add(lblName);
             Controls.Add(txtName);
+            Controls.Add(lblPassword);
             Controls.Add(txtPassword);
             Controls.Add(btnLogin);
+            Controls.Add(pictureBoxLogo);
             Name="Form1";
             Text="Login - Restaurante";
             WindowState=FormWindowState.Maximized;
-            Load+=Form1_Load;
             Resize+=Form1_Resize;
             ((System.ComponentModel.ISupportInitialize)pictureBoxLogo).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            CenterControls(); // Centra los controles al inicio
-        }
-
         private void Form1_Resize(object sender, EventArgs e)
         {
-            CenterControls(); // Reposiciona si la ventana cambia de tamaño
+            int centerX = (ClientSize.Width - 300) / 2;
+            int centerLogoX = (ClientSize.Width - 250) / 2;
+
+            pictureBoxLogo.Location = new Point(centerLogoX, 200);
+            lblName.Location = new Point(centerX, 460);
+            txtName.Location = new Point(centerX, 490);
+            lblPassword.Location = new Point(centerX, 550);
+            txtPassword.Location = new Point(centerX, 580);
+            btnLogin.Location = new Point(centerX, 660);
+
+
+
         }
-
-        private void CenterControls()
-        {
-            int centerX = ClientSize.Width / 2; // Centro horizontal
-            int centerY = ClientSize.Height / 2; // Centro vertical
-
-            // Centrar PictureBox arriba
-            pictureBoxLogo.Location = new Point(centerX - pictureBoxLogo.Width / 2, centerY - 200);
-
-            // Centrar TextBox "Izena"
-            txtName.Location = new Point(centerX - txtName.Width / 2, centerY - 40);
-
-            // Centrar TextBox "Pasahitza"
-            txtPassword.Location = new Point(centerX - txtPassword.Width / 2, centerY + 10);
-
-            // Centrar Botón "Saioa hasi"
-            btnLogin.Location = new Point(centerX - btnLogin.Width / 2, centerY + 60);
-        }
-
-
     }
 }
